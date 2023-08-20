88f9c1cc0a612b31fa06dbb1b9c3029c09621151
Rayyan365 <rayanabdallah09@gamil.com>
2023-08-20 00:14:53 -0700

    largest numbet in groups assignment done

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Good Luck 💪🏾
+function largestOfFour(arr) {
+  let answer = []
+  for (let i = 0; i < arr.length; i++) {
+    answer.push(Math.max(...arr[i]))
+  }
+  return answer
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
